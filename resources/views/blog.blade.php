6fb20eaa76b9dcb3fdc6fb3978f7475e0dc5f2c6
jondecasa <87643732+jondecasa@users.noreply.github.com>
2023-10-26 17:34:40 +0200

    mejorada vista de entradas de blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,32 +6,26 @@
 <div class="pt-5"></div>
 <div class="max-width pt-5">
     <div class="container">
+    <div class="row animacionVisible ani-slide-up animacion">
             @foreach($posts as $post)
-            <article>
-                <div class="row animacionVisible ani-slide-up animacion">
-                    <div class="col-12 col-lg-5">
+                <div class="col-12 col-lg-3">
                         @if($post->header)
-                        <a href="{{url("blog/".$post->slug)}}">
+                        <a href="{{url('blog/'.$post->slug)}}">
                             <div class="fotoNoticia">
-                                <img src="{{asset("img/posts/".$post->header->ruta)}}" alt="{{$post->header->altText ?? ""}}"/>
+                                <img src="{{asset('img/posts/'.$post->header->ruta)}}" alt="{{$post->header->altText ?? ""}}"/>
                             </div> 
                         </a>   
                         @endif
-                    </div>
-                    <div class="col-12 col-lg-7 contenedorNoticia">
                         <div class="noticia">
                             <div class="fechaTitulo">{{date('d/m/Y', strtotime($post->fechaPublicacion))}}</div>
-                            <a href="{{url("blog/".$post->slug)}}">
+                            <a href="{{url('blog/'.$post->slug)}}">
                                 <div class="noticiaTitulo">{{$post->titulo}}</div>
                             </a>
                             <div class="noticiaTexto">{{$post->resumen}}</div>
                         </div>
-                    </div>
                 </div>
-            </article>
-
-                
             @endforeach
+            </div>
             @if ($posts->hasPages())
                 <div class="pagination-wrapper">
                     {{ $posts->links() }}
